0afa235defbb475ff210449813dc7fd432ca2a80
codeeon <yeon.develop@gmail.com>
2026-03-02 23:12:17 +0900

    refactor(engine): 로더 JSON 타입 단언 구조를 명시 타입으로 정리

--- packages/domain/time-inference/src/engine/loader.ts
+++ packages/domain/time-inference/src/engine/loader.ts
@@ -2,9 +2,18 @@ import engineJson from '@workspace/engine-data/data/engine.json'
 import type { EngineQuestion, EngineSettings } from './types'
 import type { ZishiName } from '../survey/types'
 
-export const ENGINE_SETTINGS = engineJson.engine_settings as unknown as EngineSettings
-export const ZISHI_LIST = engineJson.engine_settings.zishi_list as ZishiName[]
-export const ENGINE_QUESTIONS = engineJson.questions as unknown as EngineQuestion[]
+interface EngineSettingsData extends EngineSettings {}
+interface EngineQuestionsData extends Array<EngineQuestion> {}
+interface EngineDataPayload {
+  engine_settings: EngineSettingsData
+  questions: EngineQuestionsData
+}
+
+const ENGINE_DATA = engineJson as unknown as EngineDataPayload
+
+export const ENGINE_SETTINGS = ENGINE_DATA.engine_settings
+export const ZISHI_LIST = ENGINE_SETTINGS.zishi_list as ZishiName[]
+export const ENGINE_QUESTIONS = ENGINE_DATA.questions
 
 export const QUESTION_MAP = new Map(ENGINE_QUESTIONS.map(q => [q.id, q]))
 
